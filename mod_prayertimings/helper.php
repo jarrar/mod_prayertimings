4812be0a4fb2c1eb7f7c3f8fcb68845148fe773c
Jarrar Jaffari <jjaffari@cisco.com>
2016-10-08 10:07:07 -0400

    first commit with basics in place

---
+++ mod_prayertimings/helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Helper class for Prayer Timings.
+ *
+ * @package    Iabat.PrayerTimes
+ * @subpackage Modules
+ * @license    GNU/GPL, see LICENSE.php
+ * 
+ * mod_prayertimes is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+class PrayerTimes
+{
+    /**
+     * Retrieves the hello message
+     * @param   array  $params An object containing the module parameters
+     * @access public
+     */
+    public static function get_prayer_timings($params)
+    {
+        return 'Hello, World! JARRAR';
+    }
+}
